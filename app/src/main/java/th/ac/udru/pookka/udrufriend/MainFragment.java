9cbd45099d9966d0621a5a7fd43be2be315c36f2
p00kka <sawitree@udru.ac.th>
2018-11-19 15:33:06 +0700

    replace fragment

--- app/src/main/java/th/ac/udru/pookka/udrufriend/MainFragment.java
+++ app/src/main/java/th/ac/udru/pookka/udrufriend/MainFragment.java
@@ -2,10 +2,12 @@ package th.ac.udru.pookka.udrufriend;
 
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 
 /**
@@ -13,10 +15,29 @@ import android.view.ViewGroup;
  */
 public class MainFragment extends Fragment {
 
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+//        Register Controller
+//        get value from xml id or initial view
+        TextView textView = getView().findViewById(R.id.textViewRegister);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+//                Replace Fragment
+                getActivity().getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.layoutMainFragment, new RegisterFragment()).addToBackStack(null)
+                        .commit();
+            }
+        });
+
+    }   //main method
 
     public MainFragment() {
         // Required empty public constructor
-    }
+    }   // Conductor
 
 
     @Override
@@ -24,6 +45,7 @@ public class MainFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_main, container, false);
-    }
+    }   //Create view
+
 
 }
