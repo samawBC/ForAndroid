6d093c0392fd3367cc4d430ef6bfcc49148dac8a
koikoi <samawan@gmail.com>
2018-11-21 14:03:05 +0700

    21Nov ++ koi update day3  :  Sign out Success.

--- app/src/main/java/th/ac/udru/pookka/udrufriend/ServiceActivity.java
+++ app/src/main/java/th/ac/udru/pookka/udrufriend/ServiceActivity.java
@@ -1,7 +1,14 @@
 package th.ac.udru.pookka.udrufriend;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class ServiceActivity extends AppCompatActivity {
 
@@ -9,5 +16,46 @@ public class ServiceActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_service);
+
+//        Create toolbar
+        createToolbar();
+
+
+    } // main method
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (item.getItemId() == R.id.itemExit) {
+            signOutService();
+        }
+
+        return super.onOptionsItemSelected(item);
     }
-}
+
+    private void signOutService() {
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+        firebaseAuth.signOut();
+        startActivity(new Intent(ServiceActivity.this,MainActivity.class));
+        finish();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_service,menu);
+
+        return super.onCreateOptionsMenu(menu);
+
+    }
+
+    private void createToolbar() {
+        Toolbar toolbar = findViewById(R.id.toolbarService);
+        setSupportActionBar(toolbar);
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+        FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
+        getSupportActionBar().setSubtitle(firebaseUser.getDisplayName());
+
+    } // method createToolbar
+
+
+} //main class
