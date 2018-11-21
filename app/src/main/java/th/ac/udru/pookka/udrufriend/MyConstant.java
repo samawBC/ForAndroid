2ef4f11316557835418142beabfce541c4181c2d
koikoi <samawan@gmail.com>
2018-11-21 21:43:08 +0700

    21Nov - chang icon day3  :  finish day  .

--- app/src/main/java/th/ac/udru/pookka/udrufriend/MyConstant.java
+++ app/src/main/java/th/ac/udru/pookka/udrufriend/MyConstant.java
@@ -4,9 +4,9 @@ public class MyConstant {
 
     private int[] iconInts = new int[]{
             R.drawable.ic_action_iconmenu1,
-            R.drawable.ic_action_iconmenu2,
+            R.drawable.ic_action_map,
             R.drawable.ic_action_iconmenu3,
-            R.drawable.ic_action_iconmenu4};
+            R.drawable.ic_action_exit2};
 
     private String[] titleStrings = new String[]{
             "My Friends",
