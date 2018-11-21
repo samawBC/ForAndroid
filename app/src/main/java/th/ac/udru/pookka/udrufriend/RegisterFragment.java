4d977ab9cdacd9ee910f46be9d161faa61cdae8e
Sudarat <sudarat.n@udru.ac.th>
2018-11-21 12:04:16 +0700

    day2 menu upload

--- app/src/main/java/th/ac/udru/pookka/udrufriend/RegisterFragment.java
+++ app/src/main/java/th/ac/udru/pookka/udrufriend/RegisterFragment.java
@@ -1,19 +1,33 @@
 package th.ac.udru.pookka.udrufriend;
 
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class RegisterFragment extends Fragment {
+
+//    explicit
+
+    private Uri uri;
+    private ImageView imageView;
 
 
     @Override
@@ -23,14 +37,96 @@ public class RegisterFragment extends Fragment {
 //        create toolbar
         createToolbar();
 
+//        avata controller
+        avataController();
 
     }   // main Method
+
+    //สร้าง Override Method ด้วยการกด  Alt+insert
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (item.getItemId() == R.id.itemUpload) {
+
+            return true ;
+        }
+
+        return super.onOptionsItemSelected(item);
+
+
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+
+        inflater.inflate(R.menu.menu_register,menu);
+
+
+    }
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == getActivity().RESULT_OK) {
+
+           uri = data.getData();
+
+            try {
+                //
+                Bitmap bitmap = BitmapFactory.decodeStream(getActivity().getContentResolver().openInputStream(uri));
+                //resize image
+                Bitmap bitmap1 = Bitmap.createScaledBitmap(bitmap,800,600,false);
+                imageView.setImageBitmap(bitmap1);
+
+            } catch (Exception e) {             // Exception e =การเออเร่อที่ยอมรับได้
+                e.printStackTrace();
+
+            }
+
+        } else {
+            Toast.makeText(getActivity(),
+                    "Please Choose Image",
+                    Toast.LENGTH_SHORT).show();
+        }
+
+    }  //result
+
+    private void avataController() {
+        imageView = getView().findViewById(R.id.imageViewAvartar);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType("image/*");
+                startActivityForResult(Intent.createChooser(intent,
+                        "Please choose App and Image"), 5);
+            }
+        });
+
+    }
 
     private void createToolbar() {
         Toolbar toolbar = getView().findViewById(R.id.toolbarRegister);
         ((MainActivity)getActivity()).setSupportActionBar(toolbar);
         ((MainActivity) getActivity()).getSupportActionBar().setTitle(R.string.register);
         ((MainActivity) getActivity()).getSupportActionBar().setSubtitle(R.string.title_have_space);
+        ((MainActivity) getActivity()).getSupportActionBar().setHomeButtonEnabled(true);
+        ((MainActivity) getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getActivity().getSupportFragmentManager().popBackStack();
+            }
+        });
+
+
+        setHasOptionsMenu(true);
+
     }
 
     public RegisterFragment() {
